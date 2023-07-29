dde2b3dd50685d70c080ea9cff07619387aeb5d3
MUTED64 <muted32555664@gmail.com>
2023-07-29 16:52:09 +0800

    New UI

---
+++ js/precache-manifest.d5ecd225ce7766756aae233e016e901b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0e89bfc061c16c827b9",
+    "url": "css/app.25a36952.css"
+  },
+  {
+    "revision": "31bdaede4df6efc2ac75",
+    "url": "css/chunk-vendors.ffe4e201.css"
+  },
+  {
+    "revision": "a28069ffb5b832286fe3",
+    "url": "css/music.1a6e3503.css"
+  },
+  {
+    "revision": "2ab60962cdb51941333d05d3d703c1ae",
+    "url": "img/aplause.2ab60962.jpg"
+  },
+  {
+    "revision": "1f88983aa9a4c5eff70a895a5215fd31",
+    "url": "img/background.1f88983a.jpg"
+  },
+  {
+    "revision": "1fc286fa3e29598ada3f859c9de42ccc",
+    "url": "img/default_album.1fc286fa.png"
+  },
+  {
+    "revision": "8543893a27cad8642927ee49e87a9b45",
+    "url": "img/fl.8543893a.jpg"
+  },
+  {
+    "revision": "119149683bf617e7816153fd13a55939",
+    "url": "img/logo.11914968.png"
+  },
+  {
+    "revision": "ccff59be81ef0c4bb78362a312a66da1",
+    "url": "img/uicon.ccff59be.jpg"
+  },
+  {
+    "revision": "bc9ec5962a43acc615055f2963c5a4d9",
+    "url": "index.html"
+  },
+  {
+    "revision": "e0e89bfc061c16c827b9",
+    "url": "js/app.07ad76db.js"
+  },
+  {
+    "revision": "31bdaede4df6efc2ac75",
+    "url": "js/chunk-vendors.fddf53cf.js"
+  },
+  {
+    "revision": "a28069ffb5b832286fe3",
+    "url": "js/music.e0a6f463.js"
+  },
+  {
+    "revision": "5144dcd1436c8f6a54821bb265ff7cb7",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@ importScripts("js/workbox-v4.3.1/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "js/workbox-v4.3.1"});
 
 importScripts(
-  "js/precache-manifest.426d57f1cabcb4ff234f0214ada141a3.js"
+  "js/precache-manifest.d5ecd225ce7766756aae233e016e901b.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "jusic-ui"});
